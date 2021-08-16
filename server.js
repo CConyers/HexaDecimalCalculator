d9f3a2758955f7923026fbf4f587bed9a18f0336
Campbell Conyers <campbell.conyers@madefutureproof.com>
2021-08-16 11:56:35 +0100

    updated server.js to include basic error handling

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 app.use(express.json())
 
 const calculatorRouter = require('./routes/calculations')
 app.use('/calculations', calculatorRouter)
 
-app.listen(3000, () => {
-  console.log('This server has started')
+app.listen(port, (error) => {
+  if (error) {
+    console.log('An error has occurred: ', error)
+  } else {
+    console.log('Server is listening on port: ', port)
+  }
 })
